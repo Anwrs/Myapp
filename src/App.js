565434e9b3ccb410b6ab448869ea516e1f2d0c95
Anwrs <anouar.elghazaoui@student.rocva.nl>
2020-08-27 17:35:26 +0200

    EUI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import { render } from "react-dom";
 import './App.css';
-import Header from './Components/Header';
-import Footer from './Components/Footer';
-import Main from './Components/Main';
 
 import { EuiText } from "@elastic/eui";
 import "@elastic/eui/dist/eui_theme_dark.css";
@@ -12,10 +9,7 @@ function App() {
   return (
     <html>
       <body>
-        <EuiText>Ok!</EuiText>;
-        <Header />
-        <Main />
-        <Footer />
+        <EuiText>Ok!</EuiText>
       </body>
     </html>
   ); 
