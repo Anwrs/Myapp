a97dbf3be455db87b1936863b5905077a44fbc22
Anwrs <anouar.elghazaoui@student.rocva.nl>
2020-08-27 15:22:34 +0200

    Sass

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import Main from './Components/Main';
+import '@elastic/eui/src/themes/eui/eui_colors_light.scss';
+import '@elastic/eui/src/themes/eui/eui_globals.scss';
 
 function App() {
   return (
