829866bbf7c14626a31a72adeff8738ca6a02258
Anwrs <anouar.elghazaoui@student.rocva.nl>
2020-09-01 16:21:07 +0200

    Added cards + json data

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,19 +1,51 @@
 import React from 'react';
+import Data from '../Components/Data.json';
 
 import {
    EuiPanel,
    EuiText,
+   EuiFlexGroup,
+   EuiFlexItem,
+   EuiCard,
+   EuiButton,
    EuiSpacer
 } from '@elastic/eui';
+
+const cardFooterContent = (
+   <EuiFlexGroup justifyContent="flexEnd">
+     <EuiFlexItem grow={false}>
+       <EuiButton>Go for it</EuiButton>
+     </EuiFlexItem>
+   </EuiFlexGroup>
+ );
  
 const Shop = () => {
     return (
       <div>
          <EuiSpacer size='xxl'></EuiSpacer>
          <EuiPanel>
-            <EuiText textAlign='center'>
-                  <h2>On this page you find our webshop, this will show you all our items.</h2>
-            </EuiText>
+                  <EuiFlexGroup gutterSize="l">
+               <EuiFlexItem>
+                  <EuiCard
+                  
+                  textAlign="left"
+                  image={
+                     <div>
+                      <img
+                        width="20px"
+                        src="https://source.unsplash.com/400x200/?Nature"
+                        alt="Nature"
+                        />
+                    </div>
+                  }
+                  title="Elastic in Nature"
+                  description="Example of a card's description. Stick to one or two sentences."
+                  footer={cardFooterContent}
+                  >
+
+                  </EuiCard>
+               </EuiFlexItem>
+            </EuiFlexGroup>
          </EuiPanel>
          <EuiSpacer></EuiSpacer>
       </div>
